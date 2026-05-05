572e6df04e60c578d67498988698ca32d0ffb20c
gbera23-dev <gbera23@freeuni.edu.ge>
2026-05-05 22:10:27 +0400

    Added Spring dependencies and set up the application

---
+++ GymCRMSystem/src/main/java/ApplicationConfig.java
@@ -0,0 +1,10 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@ComponentScan
+@Configuration
+public class ApplicationConfig {
+
+}
---
+++ GymCRMSystem/src/main/java/Main.java
@@ -0,0 +1,14 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+
+
+public static void main(String[] args) {
+    ApplicationContext applicationContext =
+            new AnnotationConfigApplicationContext(ApplicationConfig.class);
+}
+
+
+
+}
