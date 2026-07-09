e9310be4e71ce01b81e8ef4e613bdc58bbbeac14
gbera23-dev <gbera23@freeuni.edu.ge>
2026-07-09 20:37:19 +0400

    added initial discovery application

---
+++ GymCRMSystemDiscovery/src/test/java/com/example/GymCRMSystemDiscovery/GymCrmSystemDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GymCRMSystemDiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymCrmSystemDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
