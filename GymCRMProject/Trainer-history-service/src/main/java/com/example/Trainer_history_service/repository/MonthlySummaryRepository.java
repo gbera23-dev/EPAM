ebaa8be316f72624cbdb7e228c6b3bd6e71bd220
gbera23-dev <gbera23@freeuni.edu.ge>
2026-07-30 14:59:40 +0400

    migrated to actual param names in repository layer instead of hardcoded values

--- GymCRMProject/Trainer-history-service/src/main/java/com/example/Trainer_history_service/repository/MonthlySummaryRepository.java
+++ GymCRMProject/Trainer-history-service/src/main/java/com/example/Trainer_history_service/repository/MonthlySummaryRepository.java
@@ -5,16 +5,17 @@ import com.example.Trainer_history_service.documents.MonthlySummary;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.Update;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.Optional;
 
 public interface MonthlySummaryRepository extends MongoRepository<MonthlySummary,Long> {
 
-    @Query("{ 'trainerWorkload.$id' : ?0, 'date' : ?1 }")
-    Optional<MonthlySummary> findByTrainerWorkloadIdAndDate(String id, LocalDate date);
+    @Query("{ 'trainerWorkload.$id' : ?#{#id}, 'date' : ?#{#date} }")
+    Optional<MonthlySummary> findByTrainerWorkloadIdAndDate(@Param("id") String id, @Param("date") LocalDate date);
 
-    @Query("{ 'id' : ?0 }")
-    @Update("{ '$set' : { 'duration' : ?1 } }")
-    void updateDuration(String id, Integer duration);
+    @Query("{ 'id' : ?#{#id} }")
+    @Update("{ '$set' : { 'duration' : ?#{#duration} } }")
+    void updateDuration(@Param("id") String id, @Param("duration") Integer duration);
 }
